ff2bdcd7ca03baa2fa2f84127e577539f0701161
ryan nguyen <rnguyen513@gmail.com>
2023-08-25 19:28:53 -0400

    change arrows and disable mobile zoom

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <head>
+        <title>Ryan Nguyen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+      </head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,11 @@ export default function Home() {
       <Background></Background>
       <div className={"relative flex flex-col flex-grow min-h-fit justify-center align-center"}>
         <a href="about" className={"group flex flex-row bg-gray-100/10 sm:mx-auto sm:max-w-fit md:min-h-fit sm:rounded-2xl ring-1 ring-gray-300/10 shadow-lg shadow-white/20 p-10 pb-14 hover:bg-gray-100/5 hover:ring-gray-300/5"}>
-          <p className={"font-bold text-8xl text-white drop-shadow-2xl dark:text-white-200"}>Explore<br></br>Ryan<br></br>Nguyen &rarr;</p>
-          <p className={"absolute invisible group-hover:visible font-bold text-8xl ml-1 mt-1 text-white/50"}>Explore<br></br>Ryan<br></br>Nguyen &rarr;</p>
+          <p className={"font-bold text-8xl text-white drop-shadow-2xl dark:text-white-200"}>Explore<br></br>Ryan<br></br>Nguyen &#8674;</p>
+          <p className={"absolute invisible group-hover:visible font-bold text-8xl ml-1 mt-1 text-white/50"}>Explore<br></br>Ryan<br></br>Nguyen &#8674;</p>
         </a>
-        <a href="https://github.com/rnguyen513?tab=repositories" className={"group flex flex-row justify-center bg-gray-100/10 sm:mx-auto sm:max-w-fit md:min-h-fit sm:rounded-lg ring-1 ring-gray-300/10 shadow-lg shadow-white/20 p-5 pb-7 hover:bg-gray-100/5 hover:ring-gray-300/5 mt-5"}>
-          <p className={"font-bold text-2xl text-white drop-shadow-lg"}>See the <code className={"group-hover:text-blue-400"}>$ource c0de</code> &rarr;</p>
+        <a href="https://github.com/rnguyen513/resume" target="_blank" className={"group flex flex-row justify-center bg-gray-100/10 sm:mx-auto sm:max-w-fit md:min-h-fit sm:rounded-lg ring-1 ring-gray-300/10 shadow-lg shadow-white/20 p-5 pb-7 hover:bg-gray-100/5 hover:ring-gray-300/5 mt-5"}>
+          <p className={"font-bold text-2xl text-white drop-shadow-lg"}>See the <code className={"group-hover:text-blue-400"}>$ource c0de</code> &#8599;</p>
         </a>
       </div>
       <div className={"absolute bottom-0 right-0 h-15 w-15"}>
