5062fa9a026160c65dcc436f9893774862188471
rnguyen513 <77138030+rnguyen513@users.noreply.github.com>
2023-09-24 20:24:20 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <Background></Background>
       <div className={"relative flex flex-col flex-grow min-h-fit justify-center"}>
         <a href="about" className={"group flex flex-row bg-gray-100/5 sm:mx-auto sm:max-w-fit md:min-h-fit sm:rounded-2xl ring-1 ring-gray-300/10 shadow-lg shadow-white/20 p-10 pb-14 hover:bg-gray-100/5 hover:ring-gray-300/5"}>
-          <p className={"font-bold text-8xl text-white drop-shadow-2xl dark:text-white-200"}>Explore<br></br>Ryan<br></br>Nguyen&rarr;</p>
-          <p className={"absolute invisible group-hover:visible font-bold text-8xl ml-1 mt-1 text-white/50"}>Explore<br></br>Ryan<br></br>Nguyen&rarr;</p>
+          <p className={"font-bold text-8xl text-white drop-shadow-2xl dark:text-white-200"}>Explore<br></br>Ryan<br></br>Nguyen &rarr;</p>
+          <p className={"absolute invisible group-hover:visible font-bold text-8xl ml-1 mt-1 text-white/50"}>Explore<br></br>Ryan<br></br>Nguyen &rarr;</p>
         </a>
         <a href="https://www.linkedin.com/in/rnguyen2/" target="_blank" className={"group flex flex-row justify-center bg-gray-100/5 sm:mx-auto sm:max-w-fit md:min-h-fit sm:rounded-lg ring-1 ring-gray-300/10 shadow-lg shadow-white/20 p-5 pb-7 hover:bg-gray-100/5 hover:ring-gray-300/5 mt-5"}>
           <p className={"font-bold text-2xl text-white drop-shadow-lg"}>Ryan&apos;s <code className={"group-hover:text-blue-400"}>LinkedIn</code> &#8599;</p>
