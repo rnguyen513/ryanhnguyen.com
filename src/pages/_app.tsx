12bf20f2ae2556cbd262a039145d978b9fd2f46c
ryan nguyen <rnguyen513@gmail.com>
2024-02-11 02:19:28 -0500

    add dropdown to header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,23 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
+import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Head>
-        <title>Ryan Nguyen</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-        <link rel="shortcut icon" href="/pics/favicon.JPG"/>
-      </Head>
-      <Component {...pageProps}/>
+    <>
+      <SessionProvider session={pageProps.session}>
+        <NextUIProvider>
+          <Head>
+            <title>Ryan Nguyen</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+            <link rel="shortcut icon" href="/pics/favicon.JPG"/>
+          </Head>
+          <Component {...pageProps}/>
+        </NextUIProvider>
+      </SessionProvider>
       <Analytics/>
-    </SessionProvider>
+    </>
   )
 }
--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -24,6 +24,9 @@ export const getServerSideProps: GetServerSideProps<ConnectionStatus> = async ()
 
 */
 
+//import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Link, Button} from "@nextui-org/react"
+
 import {useSession, signIn, signOut} from "next-auth/react";
 
 export function HeaderCell({text, href, props}:any) {
@@ -36,15 +39,62 @@ export default function Header(/*{isConnected}:InferGetServerSidePropsType<typeo
 
     const {data, status} = useSession();
 
+    // return(
+    //     <Navbar className="font-bold text-3xl w-100 h-10 bg-red-200">
+    //         <NavbarBrand className="bg-purple-200">
+    //             <Link color="foreground" href="/">Ryan Nguyen</Link>
+    //         </NavbarBrand>
+    //         <NavbarContent className="hidden sm:flex gap-4 bg-blue-200">
+    //             <NavbarItem>
+    //                 <Link color="foreground" href="about">About</Link>
+    //             </NavbarItem>
+    //             <NavbarItem>
+    //                 <Link color="foreground" href="resume">Resume</Link>
+    //             </NavbarItem>
+    //             <NavbarItem>
+    //                 <Link color="foreground" href="projects">Projects</Link>
+    //             </NavbarItem>
+    //             <NavbarItem>
+    //                 <Link color="foreground" href="contact">Contact</Link>
+    //             </NavbarItem>
+    //             <NavbarItem>
+    //                 <Link href="reminders">Reminders</Link>
+    //             </NavbarItem>
+    //         </NavbarContent>
+    //         <NavbarContent className="bg-green-200">
+    //             <NavbarItem>
+    //                 <Button onClick={() => signIn("google")} color="primary" variant="flat">Sign In</Button>
+    //             </NavbarItem>
+    //         </NavbarContent>
+    //     </Navbar>
+    // )
+
     return(
         <div className="flex flex-row justify-between bg-gray-100/5 ring-0 ring-gray-300/5 shadow-lg shadow-white/10 shadow-md">
             <div className={"flex flex-row justify-start items-center h-14 pl-5 z-50"}>
-                <HeaderCell href="/" text="Ryan Nguyen"></HeaderCell>
-                <HeaderCell href="about" text="About"></HeaderCell>
-                <HeaderCell href="resume" text="Resume"></HeaderCell>
-                <HeaderCell href="projects" text="Projects"></HeaderCell>
-                <HeaderCell href="contact" text="Contact"></HeaderCell>
-                <HeaderCell href="reminders" text="Reminderz"></HeaderCell>
+                <div className="flex lg:hidden">
+                    <Dropdown className="font-bold text-3xl" backdrop="blur">
+                        <DropdownTrigger>
+                            <Button variant="bordered" className="text-3xl">&equiv;</Button>
+                        </DropdownTrigger>
+                        <DropdownMenu variant="faded" aria-label="Static Actions" className="bg-gray-500/95 ring-2 ring-gray-300/50 shadow-lg shadow-white/40 rounded-lg">
+                            <DropdownItem key="rnguyen" href="/">Ryan Nguyen</DropdownItem>
+                            <DropdownItem key="about" href="about">About</DropdownItem>
+                            <DropdownItem key="resume" href="resume">Resume</DropdownItem>
+                            <DropdownItem key="projects" href="projects">Projects</DropdownItem>
+                            <DropdownItem key="contact" href="contact">Contact</DropdownItem>
+                            <DropdownItem key="reminders" href="reminders">Reminders</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+                <div className="hidden lg:flex">
+                    <HeaderCell href="/" text="Ryan Nguyen"></HeaderCell>
+                    <HeaderCell href="about" text="About"></HeaderCell>
+                    <HeaderCell href="resume" text="Resume"></HeaderCell>
+                    <HeaderCell href="projects" text="Projects"></HeaderCell>
+                    <HeaderCell href="contact" text="Contact"></HeaderCell>
+                    <HeaderCell href="reminders" text="Reminders"></HeaderCell>
+                </div>
             </div>
             <div className="flex flex-row items-center pr-5 font-bold">
                 {(status != "authenticated") ? ((status == "loading") ? (
@@ -53,9 +103,9 @@ export default function Header(/*{isConnected}:InferGetServerSidePropsType<typeo
                     <button onClick={() => signIn("google")}>Sign in with <a className="text-green-200">Google</a>!</button>
                 )) : (
                     <>
-                        <p className="text-yellow-200 pr-2">{data.user?.name}</p>
+                        <p className="pr-2">{data.user?.name}</p>
                         <img className="pr-2" src={data.user?.image ? (data.user?.image) : ("")} height="20" width="20"></img>
-                        <button onClick={() => signOut({redirect:false})}>Sign out</button>
+                        <button onClick={() => signOut({redirect:false})} className="text-blue-200">Sign out&rarr;</button>
                     </>
                 )}
             </div>
